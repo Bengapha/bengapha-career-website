5db764a217c8a2be75a9df4b6721585ac8ed129e
Bengapha <raphaelbeluchukwu26@gmail.com>
2026-03-19 14:12:29 +0000

    setup

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_Raphael():
-  return "Hello, Nonso"
+  return "Hello, Akachukwu"
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+if __name__ =="__main__":
+  app.run(host="0.0.0.0", debug=True)
+  
+  
 
 
